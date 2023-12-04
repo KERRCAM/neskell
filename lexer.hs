b90a624d9f72f78f9bd8d9ee89b5877beedb974c
KERRCAM <kerrcameron@hotmail.com>
2023-12-04 23:22:28 +0000

    need to redo logic is bad

--- lexer.hs
+++ lexer.hs
@@ -1,3 +1,7 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Eta reduce" #-}
+{-# HLINT ignore "Avoid lambda" #-}
+{-# HLINT ignore "Use infix" #-}
 import System.IO 
 
 
@@ -6,30 +10,42 @@ getFileContents fileName =
         contents <- readFile fileName
         let seperated = lines contents
         return seperated
-       
+
+
+
 lexFile contents = map (\x -> removeWhiteSpace x) contents
 
 removeWhiteSpace line = lexLine(foldr (\x acc -> if x == ' ' then acc else x) "" line)
 
-lexLine line = map (\x -> tokenType x) line -- goes char by char 
+lexLine line = getTokens line -- goes char by char 
 
-tokenType token 
-    | token == '=' = "TOKEN_EQUALS"
-    | token == '!' = "TOKEN_EXMARK"
-    | token == '(' = "TOKEN_LPAREN"
-    | token == ')' = "TOKEN_RPAREN"
-    | token == '[' = "TOKEN_LSQUARE"
-    | token == ']' = "TOKEN_RSQUARE"
-    | token == '{' = "TOKEN_LCURL"
-    | token == '}' = "TOKEN_RCURL"
-    | token == '+' = "TOKEN_PLUS"
-    | token == '-' = "TOKEN_MINUS"
-    | token == '*' = "TOKEN_STAR"
-    | token == '%' = "TOKEN_PERCENT"
-    | token == ':' = "TOKEN_COLON"
-    | token == ';' = "TOKEN_SEMI"
-    | token == '<' = "TOKEN_LT"
-    | token == '>' = "TOKEN_GT"
-    | otherwise = "TOKEN_OTHER"
-    
+{-
+tokenType line 
+    | head line == '=' = "TOKEN_EQUALS" ++ tokenType tail line
+    | head line == '!' = "TOKEN_EXMARK"
+    | head line == '(' = "TOKEN_LPAREN"
+    | head line == ')' = "TOKEN_RPAREN"
+    | head line == '[' = "TOKEN_LSQUARE"
+    | head line == ']' = "TOKEN_RSQUARE"
+    | head line == '{' = "TOKEN_LCURL"
+    | head line == '}' = "TOKEN_RCURL"
+    | head line == '+' = "TOKEN_PLUS"
+    | head line == '-' = "TOKEN_MINUS"
+    | head line == '*' = "TOKEN_STAR"
+    | head line == '%' = "TOKEN_PERCENT"
+    | head line == ':' = "TOKEN_COLON"
+    | head line == ';' = "TOKEN_SEMI"
+    | head line == '<' = "TOKEN_LT"
+    | head line == '>' = "TOKEN_GT"
+    | otherwise = "TOKEN_UNKNOWN"
+-}
 
+getTokens (x:xs) 
+    | elem x ['a','b'..'z'] = getWord x:xs 
+
+getWord [] = []
+getWord (x:xs) 
+    | elem x ['a','b'..'z'] = x : getWord xs
+    | otherwise = getWord []
+
+--checkKeyword string
