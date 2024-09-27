bca75f7ffc3026fa023a08e469bf51f46c1734f2
KERRCAM <kerrcameron@hotmail.com>
2024-09-27 18:25:30 +0100

    remove dead

--- lexer.hs
+++ lexer.hs
@@ -5,6 +5,7 @@
 import System.IO 
 
 
+getFileContents :: FilePath -> IO [String]
 getFileContents fileName = 
     do 
         contents <- readFile fileName
@@ -12,40 +13,3 @@ getFileContents fileName =
         return seperated
 
 
-
-lexFile contents = map (\x -> removeWhiteSpace x) contents
-
-removeWhiteSpace line = lexLine(foldr (\x acc -> if x == ' ' then acc else x) "" line)
-
-lexLine line = getTokens line -- goes char by char 
-
-{-
-tokenType line 
-    | head line == '=' = "TOKEN_EQUALS" ++ tokenType tail line
-    | head line == '!' = "TOKEN_EXMARK"
-    | head line == '(' = "TOKEN_LPAREN"
-    | head line == ')' = "TOKEN_RPAREN"
-    | head line == '[' = "TOKEN_LSQUARE"
-    | head line == ']' = "TOKEN_RSQUARE"
-    | head line == '{' = "TOKEN_LCURL"
-    | head line == '}' = "TOKEN_RCURL"
-    | head line == '+' = "TOKEN_PLUS"
-    | head line == '-' = "TOKEN_MINUS"
-    | head line == '*' = "TOKEN_STAR"
-    | head line == '%' = "TOKEN_PERCENT"
-    | head line == ':' = "TOKEN_COLON"
-    | head line == ';' = "TOKEN_SEMI"
-    | head line == '<' = "TOKEN_LT"
-    | head line == '>' = "TOKEN_GT"
-    | otherwise = "TOKEN_UNKNOWN"
--}
-
-getTokens (x:xs) 
-    | elem x ['a','b'..'z'] = getWord x:xs 
-
-getWord [] = []
-getWord (x:xs) 
-    | elem x ['a','b'..'z'] = x : getWord xs
-    | otherwise = getWord []
-
---checkKeyword string
